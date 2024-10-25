8762725980d564aafee3066ec2758817346c9ff3
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-25 14:21:49 +0530

    Added  check answer fn

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,23 @@ function App() {
   const [showScore, setShowScore] = useState(false)
   const [timer, setTimer] = useState(60)
 
+  const CheckAnswer = option =>{
+    if (questionData[currentQuestion].correctOption === option)
+    {
+      setScore(score+1)
+    }
+    if (currentQuestion<questionData.length-1)
+      {
+      setCurrentQuestion(currentQuestion+1)
+      setTimer(10)
+    }else  setShowScore(true)
+  }
   return (
     <>
     <div className="quiz-app">
       {showScore ? (
         <div className="score-section">
-        <h2>Your Score : 3/3</h2>
+        <h2>Your Score : {score}/{questionData.length}</h2>
         <button>Restart</button>
       </div>) : (   
         <div className="question-section">
@@ -21,7 +32,7 @@ function App() {
         <p>{questionData[currentQuestion].question}</p>
         <div className="options">
         { questionData[currentQuestion].options.map((option,index)=>(
-          <button key={index}>{option}</button>
+          <button key={index} onClick={()=>CheckAnswer(option)}>{option}</button>
         ))}
         </div>
         <div className="timer">Time Left: 
