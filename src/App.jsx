6be842b7d01fa00ead325526f45ed7aa4ea21076
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-25 14:37:06 +0530

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import questionData from './questions.json'
 
@@ -6,26 +6,47 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [score, setScore] = useState(0)
   const [showScore, setShowScore] = useState(false)
-  const [timer, setTimer] = useState(60)
+  const [timer, setTimer] = useState(10)
 
-  const CheckAnswer = option =>{
-    if (questionData[currentQuestion].correctOption === option)
-    {
-      setScore(score+1)
-    }
+  const nextQuestion = () => {
     if (currentQuestion<questionData.length-1)
       {
       setCurrentQuestion(currentQuestion+1)
       setTimer(10)
     }else  setShowScore(true)
   }
+  const CheckAnswer = option =>{
+    if (questionData[currentQuestion].correctOption === option)
+    {
+      setScore(score+1)
+    }
+    nextQuestion()
+  }
+  const restart = ()=>{
+    setCurrentQuestion(0)
+    setScore(0)
+    setShowScore(false)
+    setTimer(10)
+  }
+  useEffect(()=>{
+    let interval;
+    if (timer>0 && !showScore) {
+      interval = setInterval(()=>{
+        setTimer(timer-1)
+      },1000)
+    }else{
+        clearInterval(interval)
+        nextQuestion()
+      }
+      return ()=> clearInterval(interval)
+  },[timer,showScore])
   return (
     <>
     <div className="quiz-app">
       {showScore ? (
         <div className="score-section">
         <h2>Your Score : {score}/{questionData.length}</h2>
-        <button>Restart</button>
+        <button onClick={restart}>Restart</button>
       </div>) : (   
         <div className="question-section">
         <h2>Question {currentQuestion+1}</h2>
