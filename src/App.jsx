d4fdbc7732fc343ed944b8c3fbdcbe2b58a3b6fc
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-25 13:04:42 +0530

    Basic HTML Completed

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="quiz-app">
+      <div className="score-section"
+      style = { {display:"none"}}>
+        <h2>Your Score : 3/3</h2>
+        <button>Restart</button>
+      </div>
+      <div className="question-section">
+        <h2>Question 1</h2>
+        <p>This is sample question</p>
+        <div className="options">
+          <button>Option-1</button>
+          <button>Option-2 </button>
+          <button>Option-3 </button>
+          <button>Option-4</button>
+        </div>
+        <div className="timer">Time Left: 5s</div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
